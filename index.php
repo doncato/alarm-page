6eea29e1ec68967d5fc5d8add50a61df772c8406
Don Cato <Don@FlatMacDon.local>
2022-02-01 16:07:00 +0100

    Updated meta info

--- index.php
+++ index.php
@@ -1,73 +1,64 @@
+<?php
+    $speed = $_POST["speed"];
+    if ($msg == "") {
+        $speed = $_GET["speed"];
+        if ($speed == "") {
+            $speed = "3000";
+        }
+    }
+    $head = $_POST["header"];
+    if ($msg == "") {
+        $head = $_GET["header"];
+        if ($head == "") {
+            $head = "DANGER!";
+        }
+    }
+    $msg = $_POST["message"];
+    if ($msg == "") {
+        $msg = $_GET["message"];
+        if ($msg == "") {
+            $msg = "ALERT!";
+        }
+    }
+    $footer = $_POST["footer"];
+    if ($footer == "") {
+        $footer = $_GET["footer"];
+        if ($footer == "") {
+            $footer = "ERROR!";
+        }
+    }
+    $no_slider = $_POST["no_slider"];
+    if ($no_slider == "") {
+        $no_slider = $_GET["no_slider"];
+        if ($no_slider == "") {
+            $no_slider = False;
+        }
+    if ($no_slider != False) {
+        $no_slider = True;
+    }
+    }
+    if ($head == "none") {
+        $head = "";
+    }
+    if ($msg == "none") {
+        $msg = "";
+    }
+    if ($footer == "none") {
+        $footer = "";
+    }
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en" xmlns="http://www.w3.org/1999/">
     <head>
-        <title>Venture</title>
+        <title><?php echo $head?></title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta name="author" content="https://github.com/doncato/alarm-page" />
         <meta name="description" content="Alert" />
 
-        <meta content="Alarm" property="og:title" />
-        <meta content="Alert!" property="og:description" />
-        <meta content="Alarm" property="og:site_name" />
-        <meta content="website" property="og:type" />
-        <meta content="#cc0000" data-react-helmet="true" name="theme-color" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <link media="screen, print" />
-        <link rel="shortcut icon" type="image/png" href="https://venture.zossennews.de/home/img/logo_light.png" />
-        <link rel="icon" type="image/png" href="https://venture.zossennews.de/home/img/logo_light.png" />
+        <link media="screen"/>
 
         <link rel="stylesheet" href="style.css">
     </head>
-    <?php
-        $speed = $_POST["speed"];
-        if ($msg == "") {
-            $speed = $_GET["speed"];
-            if ($speed == "") {
-                $speed = "3000";
-            }
-        }
-        $head = $_POST["header"];
-        if ($msg == "") {
-            $head = $_GET["header"];
-            if ($head == "") {
-                $head = "DANGER!";
-            }
-        }
-        $msg = $_POST["message"];
-        if ($msg == "") {
-            $msg = $_GET["message"];
-            if ($msg == "") {
-                $msg = "ALERT!";
-            }
-        }
-        $footer = $_POST["footer"];
-        if ($footer == "") {
-            $footer = $_GET["footer"];
-            if ($footer == "") {
-                $footer = "ERROR!";
-            }
-        }
-        $no_slider = $_POST["no_slider"];
-        if ($no_slider == "") {
-            $no_slider = $_GET["no_slider"];
-            if ($no_slider == "") {
-                $no_slider = False;
-            }
-        if ($no_slider != False) {
-            $no_slider = True;
-        }
-        }
-        if ($head == "none") {
-            $head = "";
-        }
-        if ($msg == "none") {
-            $msg = "";
-        }
-        if ($footer == "none") {
-            $footer = "";
-        }
-    ?>
     <body style="animation-duration: <?php echo $speed."ms" ?>;">
         <div class="upper">
             <div class="head inverse_anim" style="animation-duration: <?php echo $speed."ms" ?>;">
